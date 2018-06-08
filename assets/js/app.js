c97be255a54bccb48d63d2da1194baeb9d5a9e54
ITUNU <hitunes247@gmail.com>
2018-06-08 20:11:57 +0100

    page2 functionality

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+
+var checkbox = Array.from(document.getElementsByClassName('checkbox'));
+console.log(checkbox);
+checkbox.forEach(function (check) {
+  check.addEventListener('click', function (e) {
+    var text = e.target.parentElement.previousElementSibling;
+    (check.checked == true) ? text.style.color = '#0090f7' : text.style.color = '#a4aab3';
+  })
+})
+
+// document.querySelectorAll('input[type="ch"]')
